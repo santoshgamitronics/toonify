c4083508116ab1fd23060914ef11ded1be86e09b
santosh kumar m <santu279@gmail.com>
2021-10-28 00:35:03 +0530

    added backend url

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -50,7 +50,7 @@ function Home() {
             path = '/nsfwDetector'
         }
 
-        fetch(`http://localhost:8080/api${path}`, requestOptions)
+        fetch(`http://34.204.6.188/api${path}`, requestOptions)
           .then(response => response.json())
           .then(result => {
               if(result.status) {
